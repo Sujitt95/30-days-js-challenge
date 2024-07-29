c674b7c06cd3047bc05a7e898f4aa46116a75f41
Sujit kumar <sujittkumar11@gmail.com>
2024-07-29 10:13:05 +0530

    day 2 commit 1

--- Day-02.js
+++ Day-02.js
@@ -2,4 +2,21 @@
 const add=(a,b)=>{
     return a+b
 }
-console.log(add(5,3));
+const sub=(a,b)=>{
+    return a-b
+}
+const mult=(a,b)=>{
+    return a*b
+}
+const divd=(a,b)=>{
+    return a/b
+}
+const reman=(a,b)=>{
+    return a%b
+}
+console.log(`5+3=${add(5,3)}`);
+console.log(`5-3=${sub(5,3)}`);
+console.log(`4*2=${mult(4,2)}`);
+console.log(`6/3=${divd(6,3)}`);
+console.log(`remainder of 5/2 is ${reman(5,2)} `);
+
